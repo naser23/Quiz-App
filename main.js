cc7c5423c092b3b8740fd6596440cccfce29d6ac
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-12-15 16:09:32 -0500

    almost finished all the styling for the game building page. just need to add some javascript to make the beginning button appear when they select at least one option.

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 "use strict";
 
+let questionAmountOptions = document.getElementsByClassName("num-of-questions");
+console.log(questionAmountOptions);
+
 let apiUrl =
   "https://opentdb.com/api.php?amount=10&category=24&difficulty=easy&type=boolean";
+
+function createUrl(amount, category, difficulty, type) {
+  let unfinishedUrl = "https://opentdb.com/api.php?";
+}
 
 fetch(apiUrl)
   .then((response) => response.json())
