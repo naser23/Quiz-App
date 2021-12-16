1157f7076e41098946d0c82f9bed6d391328555f
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-12-16 13:14:19 -0500

    swapped names of select tags with id names. made it easier for me to select them with javascript. made the start game button appear when the number of questions value has changed. now got to make it change when the other dropdown box values change.

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 "use strict";
+// global variables
+const numOfQuestions = document.querySelector("#num-of-questions");
+const questionAmountOptions = document.getElementsByName("dropdown");
+const dropdown = document.getElementsByName("dropdown");
+const startGame = document.querySelector(".start-game");
+const options = numOfQuestions.options;
 
-let questionAmountOptions = document.getElementsByClassName("num-of-questions");
-console.log(questionAmountOptions);
-
+console.log(options);
 let apiUrl =
   "https://opentdb.com/api.php?amount=10&category=24&difficulty=easy&type=boolean";
 
@@ -15,3 +19,13 @@ fetch(apiUrl)
   .then(function (data) {
     console.log(data);
   });
+
+numOfQuestions.addEventListener("change", optionSelected);
+
+function optionSelected() {
+  for (let i = 1; i < options.length; i++) {
+    numOfQuestions.textContent !== options[i].length
+      ? (startGame.style.display = "block")
+      : console.log("they are the same.");
+  }
+}
